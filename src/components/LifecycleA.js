d9e68d8e70d3d5d2906dc81c0165c180c64062f0
PaviChandar <96729523+PaviChandar@users.noreply.github.com>
2022-04-29 16:59:37 +0530

    Update LifecycleA.js

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -18,16 +18,36 @@ class LifecycleA extends Component {
     componentDidMount() {
         console.log('Lifecycle A componentdidmount')
     }
+    
+     shouldComponentUpdate() {
+        console.log('LifecycleA shouldComponentUpdate');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('LifecycleA getSnapshotBeforeUpdate');
+        return null
+    }
+
+    componentDidUpdate() {
+        console.log('LifecycleA componentDidUpdate');
+    }
+
+    changeState = () => {
+        this.setState({
+            name : 'Viswas'
+        })
+    }
     render() {
          
             console.log('Lifecycle A render')
-            return  <div>Lifecycle A</div>
-            // return(
-            //     <div>
-            //         <div>Lifecycle A</div>
-            //         <LifecycleB />
-            //     </div>
-            // )
+             return(
+                <div>
+                     <div>Lifecycle A</div>
+                       <button onClick={this.changeState}>Click</button>
+                       <LifecycleB />
+                </div>
+             )
         
     }
 }
